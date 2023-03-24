c033cc8833ee06f4b9450f7535e88fd57895ce85
Caroline Robinson <CRobinsonAtWork@gmail.com>
2023-03-24 09:11:53 -0800

    correcting version format

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.EnforcingChildCustody',
-      version='2023-03-24',
+      version='2023.03.24',
       description=('Enforcing child custody action plan'),
       long_description='# docassemble.Enforcingchildcustody\r\n\r\nEnforcing child custody action plan\r\n\r\n## Author\r\n\r\nCaroline Robinson\r\nJeannie Sato\r\n\r\n',
       long_description_content_type='text/markdown',
